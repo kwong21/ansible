2a9a11d7a4006f683e677d20db78802c078a571b
Kelvin Wong <kelvin.wong1@outlook.com>
2020-05-02 18:20:42 -0700

    Updated deploy script to allow deloying the container only

--- deploy-card-stock.sh
+++ deploy-card-stock.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
+set -e
+set -u
+set -o pipefail
 
-VER=$1
+VER=""
+TAG=""
 
-ansible-playbook -i production.ini card-stock.yml --extra-vars "version=$VER"
+usage() {
+    echo "Script usage: $(basename "$0") [-v version to deploy] [-c deploy container only]" >&2
+    exit 1
+}
+
+while getopts ':v:c' OPTION; do
+    case "${OPTION}" in
+        v)
+            VER="$OPTARG"
+            ;;
+        c)
+            TAG="-t deploy_card_stock_container"
+            ;;
+        ?)
+            usage
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+if [ -z "${VER}" ]; then
+    usage
+fi
+
+ansible-playbook -i production.ini card-stock.yml "$TAG" --extra-vars "version=$VER"
